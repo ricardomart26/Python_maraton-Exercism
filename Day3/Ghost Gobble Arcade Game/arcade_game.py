243a5158626fac24715c91b98c6432979d10fede
ricardomart26 <ricardomartins2626t@gmail.com>
2021-09-27 13:33:37 +0100

    Bool lerning, and conditional amost done, trying to figure out logic

---
+++ Day3/Ghost Gobble Arcade Game/arcade_game.py
@@ -0,0 +1,46 @@
+def eat_ghost(power_pellet_active, touching_ghost):
+    """
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost:  bool - is the player touching a ghost?
+    :return: bool
+    """
+    if power_pellet_active == True and touching_ghost == True:
+        return True
+    elif touching_ghost == False:
+        return False 
+    return False
+
+
+def score(touching_power_pellet, touching_dot):
+    """
+    :param touching_power_pellet: bool - does the player have an active power pellet?
+    :param touching_dot:  bool - is the player touching a dot?
+    :return: bool
+    """
+    if touching_power_pellet == True or touching_dot == True:
+        return True
+    return False
+
+
+def lose(power_pellet_active, touching_ghost):
+    """
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost: bool - is the player touching a ghost?
+    :return: bool
+    """
+    if touching_ghost == True and power_pellet_active == False:
+        return True
+    return False
+
+
+def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
+    """
+
+    :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost:  bool - is the player touching a ghost?
+    :return: bool
+    """
+    if lose(power_pellet_active, touching_ghost) == True:
+        return False
+    return has_eaten_all_dots
---
+++ Day3/Meltdown Mitigation/conditionals.py
@@ -0,0 +1,73 @@
+""" Meltdown Mitigation exercise """
+
+def is_criticality_balanced(temperature, neutrons_emitted):
+    """Verify criticality is balanced.
+    :param temperature: temperature value (integer or float)
+    :param neutrons_emitted: number of neutrons emitted per second (integer or float)
+    :return:  boolean True if conditions met, False if not
+
+    A reactor is said to be critical if it satisfies the following conditions:
+    - The temperature is less than 800.
+    - The number of neutrons emitted per second is greater than 500.
+    - The product of temperature and neutrons emitted per second is less than 500000.
+    """
+    if temperature >= 800 or neutrons_emitted <= 500:
+        return False
+    elif temperature * neutrons_emitted >= 500000:
+        return False
+    return True
+
+def reactor_efficiency(voltage, current, theoretical_max_power):
+    """Assess reactor efficiency zone.
+    :param voltage: voltage value (integer or float)
+    :param current: current value (integer or float)
+    :param theoretical_max_power: power that corresponds to a 100% efficiency (integer or float)
+    :return: str one of 'green', 'orange', 'red', or 'black'
+    
+    Efficiency can be grouped into 4 bands:
+    1. green -> efficiency of 80% or more,
+    2. orange -> efficiency of less than 80% but at least 60%,
+    3. red -> efficiency below 60%, but still 30% or more,
+    4. black ->  less than 30% efficient.
+    The percentage value is calculated as
+    (generated power/ theoretical max power)*100
+    where generated power = voltage * current
+    """
+    generated_power = voltage * current
+    efficiency = (generated_power/theoretical_max_power) * 100
+    if efficiency >= 80:
+        return "green"
+    elif efficiency >= 60:
+        return "orange"
+    elif efficiency >= 30:
+        return "red"
+    else:
+        return "black"
+
+
+def fail_safe(temperature, neutrons_produced_per_second, threshold):
+    """Assess and return safety range.
+    :param temperature: value of the temperature (integer or float)
+    :param neutrons_produced_per_second: neutron flux (integer or float)
+    :param threshold: threshold (integer or float)
+    :return: str one of: 'LOW', 'NORMAL', 'DANGER'
+
+    - `temperature * neutrons per second` < 40% of `threshold` == 'LOW'
+    - `temperature * neutrons per second` +/- 10% of `threshold` == 'NORMAL'
+    - `temperature * neutrons per second` is not in the above-stated ranges == 'DANGER'
+    """
+    gen = temperature * neutrons_produced_per_second
+    per = (gen * 100) / threshold
+	
+    print(per)
+    
+    if per < 40 and per > 10:
+        return "LOW"
+    elif per < 0 or per <= 10:
+        return "NORMAL"
+    else:
+        return "DANGER"
+
+print(fail_safe(10, 899, 10000)) # Danger
+print(fail_safe(10, 1, 10000)) # LOW
+print(fail_safe(10, 0, 10000)) # LOW
