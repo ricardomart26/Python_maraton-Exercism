6717e34940e05209c33e8131daccce46e866d089
ricardomart26 <ricardomartins2626t@gmail.com>
2021-09-27 14:27:51 +0100

    Done Two fer, and learned Default arguments concept, and added some usefull links

---
+++ Day3/Two fer/two_fer.py
@@ -0,0 +1,8 @@
+def two_fer(name=""):
+    if len(name) != 0:
+        return "One for " + name + ", one for me."
+    return "One for you, one for me."
+
+#https://realpython.com/python-optional-arguments/
+#https://appdividend.com/2020/11/17/how-to-check-if-string-is-empty-or-not-in-python/
+#https://blog.finxter.com/python-default-arguments/
