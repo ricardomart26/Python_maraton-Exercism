845a3a9b1fb2d3b9563827263f212fb61d13312c
ricardomart26 <ricardomartins2626t@gmail.com>
2021-10-13 13:37:59 +0100

    finished binary search

--- Day 6/Binary Search/binary_search.py
+++ Day 6/Binary Search/binary_search.py
@@ -1,16 +1,28 @@
 import math
+import unittest
 
 def find(search_list, value):
+    lower = 0
+    size = len(search_list)
+    heigher = size - 1
+    if search_list is [] or value not in search_list:
+        raise ValueError("not found.")
     while (True):
-        size = len(search_list)
-        middle = math.floor(size / 2)
+        middle = math.floor((lower + heigher) / 2)
         if search_list[middle] == value:
             return middle
-        elif search_list[middle] > value:
-            search_list = search_list[:middle]
-        elif search_list[middle] < value:
-            search_list = search_list[middle:]
-        else:
-            return "Not found."
+        elif search_list[middle] > value and middle != 0:
+            if lower == heigher:
+                break;
+            heigher = middle - 1
+        elif search_list[middle] < value and middle != 0:
+            if lower == heigher:
+                break;
+            lower = middle + 1
+    raise ValueError("not found.")
 
-print(find([1, 2, 3, 4, 5, 6, 8], 1))
+
+
+# print(find([1, 3, 4, 6, 8, 9, 11], 11))
+# print(find([1, 2, 3, 4, 5, 6, 8], 1))
+print(find([1, 3, 4, 6, 8, 9, 11], 7))
