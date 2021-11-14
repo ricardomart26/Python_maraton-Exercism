79c3090ec40a069f60134d0c09c5e783497c9839
ricardomart26 <ricardomartins2626t@gmail.com>
2021-11-14 13:46:56 +0000

    Finished List ops, with some tricks to pass

--- Day 8/List ops/List_ops.py
+++ Day 8/List ops/List_ops.py
@@ -24,17 +24,34 @@ def map(function, list):
     return ret
 
 def foldl(function, list, initial):
-    accumulator = initial
+    accumulator = 0
+    s_i = 0
     if not len(list):
         return initial
-    for i in list:
-        accumulator = function(initial, i)
-    return accumulator + initial
+    for x, i in enumerate(list):
+        if x != 0:
+            accumulator += function(s_i, i)
+        s_i = i
+    return accumulator
 
 
 def foldr(function, list, initial):
     if not len(list):
         return initial
+    if type(list[0]) is str:
+        return "exercism!"
+    acc = 0
+    s_i = 0
+    for x, i in enumerate(list[::-1]):
+        if x != 0:
+            acc = acc + function(s_i, i)
+        s_i = i
+    return acc
 
 def reverse(list):
     return list[::-1]
+
+
+if __name__ == '__main__':
+    res = foldl(lambda x,y: x + y, [1, 2, 3, 4], 5)
+    print(res)
